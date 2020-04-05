f84bdefbd70f75ea955ba8560776d603add6fc69
KrzysztofLen <len.krzysztof@gmail.com>
2020-04-05 17:24:34 +0200

    add minify & cashing

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ element.appendChild(myIcon);
 
 const p2 = document.createElement('p');
 const numbers1 = [1, 2, 3, 4, 5, 6];
-const numbers2 = [7, 8, 9, 10];
+const numbers2 = [7, 8, 9, 10, 11];
 const numbers3 = [...numbers1, ...numbers2];
 p2.textContent = numbers3.join(' ');
 document.body.appendChild(p2);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,7 @@
 const merge = require('webpack-merge');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const commonConfig = require('./webpack.config.common');
 
 module.exports = merge(commonConfig, {
@@ -18,4 +20,21 @@ module.exports = merge(commonConfig, {
       filename: '[name].[contenthash].css',
     }),
   ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({
+        cssProcessorOptions: {
+          map: {
+            inline: false,
+            annotation: true,
+          },
+        },
+      }),
+      new TerserPlugin({
+        parallel: true,
+        cache: true,
+        sourceMap: true,
+      }),
+    ],
+  },
 });
